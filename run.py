c785a88d4f64cbb0c48d67a9474b9393e9f2afaa
Fernando <fernando.palop.12.18@gmail.com>
2026-06-12 22:12:44 +0200

    Configurar variables de entorno

--- run.py
+++ run.py
@@ -6,12 +6,13 @@ from app.routes.tareas_routes import tareas_bp
 from app.routes.auth_routes import auth_routes_bp
 from app.routes.dashboard_routes import dashboard_bp
 from app.routes.perfil_routes import perfil_bp
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
-app.secret_key = "1,2,3,4,5"
-app.config["SQLALCHEMY_DATABASE_URI"] = (
-    "postgresql://postgres:Fernando1721998@localhost:5432/mini_web_db"
-)
+load_dotenv()
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 db.init_app(app)
 app.register_blueprint(tareas_bp)
 app.register_blueprint(auth_routes_bp)
